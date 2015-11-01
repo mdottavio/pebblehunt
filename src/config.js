5f426dfe739576290267d69a7db110df3ceb23d8
dottavio <dottavio@gmail.com>
2015-11-01 11:51:38 -0300

    Moving files

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   apiBaseUrl :  'https://api.producthunt.com/v1/',
   phDevToken :  '4a61f7007d596706954baafeff4e0ef0c29377e6049ee4327d1ce67dc27ea6dd',
   images : {
-    error: ''
+    error: 'images/error.png'
   },
   useFullScreen : true
 };
