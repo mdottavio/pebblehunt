4b4cb58f55c38cde5515c587d9b40951bed50423
dottavio <dottavio@gmail.com>
2015-11-01 20:40:19 -0300

    Init app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+var UI = require('ui');
 var config = require('./config');
+var errorCard = require('./errorCard');
+var ph = require('./phunt-reader');
+
+var main = new UI.Card({
+  title: 'PebbleHunt',
+  icon: 'images/logo.png',
+  subtitle: 'fetching the latest PHunt posts',
+  body: ''
+});
+
+ph.posts(showIndex, showError);
+
+
+var showIndex = function(data){
+  console.log('We have data');
+};
+
+var showError = function(err){
+  console.log('We have an error', err);
+  errorCard.show('Ups, something went wrong...');
+};
+
--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
   apiBaseUrl :  'https://api.producthunt.com/v1/',
   phDevToken :  '4a61f7007d596706954baafeff4e0ef0c29377e6049ee4327d1ce67dc27ea6dd',
   images : {
+    app: 'images/logo.png',
     error: 'images/error.png'
   },
   useFullScreen : true
